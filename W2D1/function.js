09ce2de104ba7bd0419b510318d260f7eb3dd3bc
dianazedngl <117115917+dianazedngl@users.noreply.github.com>
2022-11-08 13:38:18 -0600

    first

---
+++ W2D1/function.js
@@ -0,0 +1,16 @@
+//1. Function to calculate sum of all the elements in an array .... using reduce method
+function sum(input){
+ return input.reduce((x,y)=>x+y);
+}
+//2. Function to calculate product of all the elements in an array .... using reduce method
+function multiply(input){
+    return input.reduce((x,y)=>x*y); 
+}
+//3. Function that reverses a string .... using map method
+function reverseString(input){
+    return input.split("").map(val=>val).reverse().join("");
+}
+//4. Function that returns words whose length is greater than "i" .... using filter method
+function filterLongWords(arr, i){
+    return arr.filter(a=>a.length>i);
+}
